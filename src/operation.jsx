3d7ed0535e3c50bbcc05f2cb567056d6a70a15e5
olaz <o.zaprzalka@gmail.com>
2021-03-18 23:45:30 +0100

    add operation delete task

--- src/operation.jsx
+++ src/operation.jsx
@@ -1,45 +1,131 @@
 import API_KEY, { API_URL } from "./constant.js";
 import React, { useState, useEffect } from "react";
 
-const GetOperations = ({id}) => {
-    const [operation, getOperation] = useState([]);
-    useEffect(() => {
+const GetOperations = ({ id, operationForm }) => {
+  const [operation, getOperation] = useState([]);
+  const [toggle, setToggle] = useState(operationForm);
+  const [addOperation, setOperation] = useState([]);
+  console.log('operationForm: ', operationForm);
+  useEffect(() => {
+      setToggle(operationForm)
+  }, [])
+  useEffect(() => {
+    fetch(`${API_URL}/tasks/${id}/operations`, {
+      headers: {
+        Authorization: API_KEY,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        getOperation(data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  const createOperation = () => {
+      const newData = {
+          description : addOperation,
+          timeSpent: 0,
+      }
       fetch(`${API_URL}/tasks/${id}/operations`, {
+        method: "POST",
+        body: JSON.stringify(newData),
         headers: {
           Authorization: API_KEY,
+          "Content-Type": "application/json",
         },
       })
         .then((response) => response.json())
         .then((data) => {
-          getOperation(data.data);
+          setOperation([...operation, data]);
+          console.log(operation)
         })
         .catch((error) => {
-          console.log(error);
+          console.log("add new error", error);
         });
-    }, []);
-  
-    if (operation) {
-      return (
-        <div>
-          <ul>
-            {operation.map((el, index) => {
-              return (
-                <li
-                  key={index}
-                  style={{ display: "flex", flexDirection: "column", margin: '20px', color: 'magenta'}}
-                >
-                  {console.log(operation)}
-                  <span>{el.timeSpent}</span>
-                  <span>{el.addedDate}</span>
-                  <span>{el.id}</span>
-                  <span>{el.description}</span>
-                </li>
-              );
-            })}
-          </ul>
+  }
+//   if (form === true) {
+//       return (
+//         <input type="text"
+//         className="form-control"
+//         placeholder="Operation description"
+//         // value={operationDescription}
+//         onChange={e => setOperationDescription(e.target.value)}/>
+//       )
+//   }
+  const renderAddNew = () => {
+    if (operationForm === true) {
+        return (
+          <div>{console.log('form', operationForm)}
+          <input type='text' placeholder='description' visibility='visible' name={addOperation} onChange={e => setOperation(e.target.value)}></input>
+          <button onClick={createOperation}>Add operation</button>
         </div>
-      );
+        )
     }
-  };
+    else {
+        return null;
+    }
+  }
+  const renderOperationList = () => {
+    if (operation) {
+        return (
+          <div>
+            <ul>{console.log(operation)}
+              {operation.map((el, index) => {
+                return (
+                  <li
+                    key={index}
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      margin: "20px",
+                      color: "green",
+                    }}
+                  >
+                    {console.log(operation)}
+                    <span>{el.timeSpent}</span>
+                    <span>{el.addedDate}</span>
+                    <span>{el.id}</span>
+                    <span>{el.description}</span>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        );
+      }
+      else {
+          return (
+              <div>No operations to render</div>
+          )
+      }
+  }
+  return (
+      <>
+      {renderAddNew()}
+      {renderOperationList()}
+      </>
+  )
+};
+
+// const addOperation = ({ id }) => {
+//     const [newOperation, addNewOperation] = ([])
+//   useEffect(() => {
+//     fetch(`${API_URL}/tasks/${id}/operations`, {
+//       method: "POST",
+//       headers: {
+//         Authorization: API_KEY,
+//       },
+//     })
+//       .then((response) => response.json())
+//       .then((data) => {
+//         addNewOperation(data.data);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }, []);
+// };
 
 export default GetOperations;
--- src/task.jsx
+++ src/task.jsx
@@ -1,90 +1,131 @@
 import API_KEY, { API_URL } from "./constant.js";
 import React, { useState, useEffect } from "react";
-import GetOperations from './operation';
+import GetOperations from "./operation";
 
 const NewTasks = () => {
-  const [title, setTitle] = useState('a kurwa');
-  const [description, setDescription] = useState('pierdole');
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   const handleTitle = (e) => {
     e.preventDefault();
-    setTitle(e.target.value)
-  }
+    setTitle(e.target.value);
+  };
   const handleDescription = (e) => {
     e.preventDefault();
-    setDescription(e.target.value)
-  }
+    setDescription(e.target.value);
+  };
   const createTask = () => {
     const newTask = {
       title: title,
       description: description,
-      status: 'open',
-    }
-    useEffect(() => {
-      fetch(`${API_URL}/tasks`, {
-        method: 'POST',
-        headers: {
-          Authorization: API_KEY,
-          "Content-Type": "application/json",
-          body: JSON.stringify(newTask)
-        },
-      })
-      .then(response => response.json())
-      .then(task => {
-        console.log(newTask)
-        console.log('task', task);
-      })
-      .catch(error => {
-        console.log('add new error', error);
-      });
-    }, [title, description])
-  }
-  const handleButton = (e) =>
-    e.preventDefault();
-    createTask();
-  return (
-    <form style={{display: 'flex', flexDirection: 'column'}}>
-      <h1>New task</h1>
-      <input type="text" value={title} onChange={handleTitle}></input>
-      <input type="text" value={description} onChange={handleDescription}></input>
-      <button type="click" onClick={handleButton}>Add task</button>
-    </form>
-  )
-}
-
-const GetTasks = () => {
-  const [data, getData] = useState([]);
-  useEffect(() => {
+      status: "open",
+    };
     fetch(`${API_URL}/tasks`, {
+      method: "POST",
+      body: JSON.stringify(newTask),
       headers: {
         Authorization: API_KEY,
+        "Content-Type": "application/json",
       },
     })
       .then((response) => response.json())
-      .then((data) => {
-        getData(data.data);
+      .then((task) => {
+        console.log("task", task);
       })
       .catch((error) => {
-        console.log(error);
+        console.log("add new error", error);
       });
-  }, []);
+  };
+  const handleButton = (e) => {
+    e.preventDefault();
+    createTask();
+    setTitle("");
+    setDescription("");
+  };
 
-  if (data) {
+  return (
+    <form style={{ display: "flex", flexDirection: "column" }}>
+      <h1>New task</h1>
+      <input type="text" value={title} onChange={handleTitle}></input>
+      <input
+        type="text"
+        value={description}
+        onChange={handleDescription}
+      ></input>
+      <button type="submit" onClick={handleButton}>
+        Add task
+      </button>
+    </form>
+  );
+};
+
+const GetTasks = () => {
+  const [data1, getData] = useState([]);
+  const [operationForm, setOperationForm] = useState(false);
+    const getData1 = () => {
+      useEffect(() => {
+        fetch(`${API_URL}/tasks`, {
+          headers: {
+            Authorization: API_KEY,
+          },
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            getData(data.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }, [])
+      return data1;
+    }
+
+  const toggleOperationForm = () => {
+    setOperationForm(prevState => !prevState);
+    console.log(operationForm)
+  };
+  const removeData = (id) => {
+      fetch(`${API_URL}/tasks/${id}`, {
+        method: 'DELETE',
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: API_KEY,
+        },
+      })
+        .then((response) => {
+          let del = data1.filter(el => id !== el.id)
+          getData(del)
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    return data1;
+  }
+
+  getData1();
+  if (data1) {
     return (
-      <div>
+      <div >
         <ul>
-          {data.map((el, index) => {
+          {data1.map((el, index) => {
             return (
               <li
                 key={index}
-                style={{ display: "flex", flexDirection: "column" }}
+                style={{ display: "flex", flexDirection: "column", color: 'magenta'}}
               >
-                {console.log(data)}
-                <span>{el.title}</span>
+                <div className="taskTitle" style={{ display: "flex", flexDirection: "row"}}>
+                  <span>{el.title}</span>
+
+                  <div className="taskBtns" style={{marginLeft: '50px'}}>
+                    <button onClick={toggleOperationForm}>Add</button>
+                    <button type="submit" onClick={() => removeData(el.id)}>Finish</button>
+                  </div>
+                </div>
+
                 <span>{el.description}</span>
                 <span>{el.addedDate}</span>
                 <span>{el.id}</span>
                 <span>{el.status}</span>
-                <GetOperations id={el.id}></GetOperations>
+                <GetOperations id={el.id} operationForm={operationForm}></GetOperations>
               </li>
             );
           })}
@@ -95,4 +136,4 @@ const GetTasks = () => {
 };
 
 export default GetTasks;
-export {NewTasks};
+export { NewTasks };
